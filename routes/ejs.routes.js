5f4d75e3c2450b68bb369289433136848ae5e54a
six-ju <yuk1479@naver.com>
2024-11-18 17:56:18 +0900

    [UPDATE] 공통 css 수정/ app.js public추가/ 라우터 수정

--- routes/ejs.routes.js
+++ routes/ejs.routes.js
@@ -5,7 +5,15 @@ const app = express();
 
 /* View Mapping */
 router.get('/login', (req, res) => {
+    console.log(req)
+    // if (req) {
+    //     return res.redirect('/');
+    // }
     res.render('index.ejs', { components: 'login' });
+});
+
+router.get('/', (req, res) => {
+    res.render('index.ejs', { components: 'user' });
 });
 
 module.exports = router;
