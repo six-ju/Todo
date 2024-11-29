8d32892609044a6dc561f74a2454f2ee4a996a33
sixju <yuk1479@naver.com>
2024-11-30 01:31:57 +0900

    [UPDATE] 클릭한번에 case문을 통해서 완료유무를 체크 및 리스트 입력은 10개 제한

--- repositories/todolist.repository.js
+++ repositories/todolist.repository.js
@@ -1,49 +1,65 @@
-const db = require('../database/db');
-const { promisePool } = require('../config/database');
+const db = require("../database/db");
+const { promisePool } = require("../config/database");
 
 class todolistRepository {
-    async getUserInfo(id) {
-        try {
-            const [data] = await promisePool.query('select * from TODOLIST where userId = ? ', id);
+  async getUserInfo(id) {
+    try {
+      const [data] = await promisePool.query(
+        "select * from TODOLIST where userId = ? ",
+        id
+      );
 
-            return data;
-        } catch (err) {
-            console.error('Query error:', err);
-        }
+      return data;
+    } catch (err) {
+      console.error("Query error:", err);
     }
+  }
 
-    async savetoDolist(listData, userName) {
-        try {
-            const [data] = await promisePool.query(
-                'INSERT INTO TODOLIST (userId,content,startDate,createdBy,updatedBy) VALUES (?,?,?,?,?) ',
-                [listData.userId, listData.content, listData.startDate, userName, userName],
-            );
+  async savetoDolist(listData, userName) {
+    try {
+      const [data] = await promisePool.query(
+        "INSERT INTO TODOLIST (userId,content,startDate,createdBy,updatedBy) VALUES (?,?,?,?,?) ",
+        [
+          listData.userId,
+          listData.content,
+          listData.startDate,
+          userName,
+          userName,
+        ]
+      );
 
-            return data;
-        } catch (err) {
-            console.error('Query error:', err);
-        }
+      return data;
+    } catch (err) {
+      console.error("Query error:", err);
     }
+  }
 
-    async deletedTodoList(id) {
-        try {
-            const [data] = await promisePool.query('DELETE FROM TODOLIST WHERE ID = ? ', id);
+  async deletedTodoList(id) {
+    try {
+      const [data] = await promisePool.query(
+        "DELETE FROM TODOLIST WHERE ID = ? ",
+        id
+      );
 
-            return data;
-        } catch (err) {
-            console.error('Query error:', err);
-        }
+      return data;
+    } catch (err) {
+      console.error("Query error:", err);
     }
+  }
 
-    async completetoDo(id) {
-        try {
-            const [data] = await promisePool.query('UPDATE TODOLIST SET COMPLETEDATE = CURDATE() WHERE ID = ? ', id);
+  // CASE 문으로 통한 조건문 작성
+  async completetoDo(id) {
+    try {
+      const [data] = await promisePool.query(
+        "UPDATE TODOLIST SET COMPLETEDATE = CASE WHEN COMPLETEDATE IS NULL THEN CURDATE() ELSE NULL END WHERE ID = ? ",
+        id
+      );
 
-            return data;
-        } catch (err) {
-            console.error('Query error:', err);
-        }
+      return data;
+    } catch (err) {
+      console.error("Query error:", err);
     }
+  }
 }
 
 module.exports = todolistRepository;
